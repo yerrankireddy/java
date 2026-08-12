dff2e5a52fa76a15dfe201b9979eba730845b87e
reddy <vtu32886@veltech.edu.in>
2026-08-12 11:24:19 +0530

    Add files via upload

---
+++ javasort.java
@@ -0,0 +1,62 @@
+
+import java.util.*;
+
+class Student {
+
+    private int id;
+    private String fname;
+    private double cgpa;
+
+    public Student(int id, String fname, double cgpa) {
+        super();
+        this.id = id;
+        this.fname = fname;
+        this.cgpa = cgpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        List<Student> studentList = new ArrayList<Student>();
+        while (testCases > 0) {
+            int id = in.nextInt();
+            String fname = in.next();
+            double cgpa = in.nextDouble();
+
+            Student st = new Student(id, fname, cgpa);
+            studentList.add(st);
+
+            testCases--;
+        }
+
+        // Sorting using Java 8 Comparators based on the requirements:
+        // 1. CGPA decreasing order
+        // 2. First name ascending order if CGPAs are equal
+        // 3. ID ascending order if first names are also equal
+        studentList.sort(
+                Comparator.comparing(Student::getCgpa).reversed()
+                        .thenComparing(Student::getFname)
+                        .thenComparing(Student::getId)
+        );
+
+        for (Student st : studentList) {
+            System.out.println(st.getFname());
+        }
+    }
+}
