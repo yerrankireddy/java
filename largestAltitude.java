efbe9e622fc17fff4a64463f2b5edf94f8be46e4
reddy <vtu32886@veltech.edu.in>
2026-08-19 11:19:16 +0530

    Add files via upload

---
+++ largestAltitude.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        
+        int maxAltitude = 0;
+        int currAltitude = 0;
+        
+        for (int g : gain) {
+            currAltitude += g;
+            maxAltitude = Math.max(maxAltitude, currAltitude);
+        }
+        
+        return maxAltitude;
+    
+
+    }
+}
